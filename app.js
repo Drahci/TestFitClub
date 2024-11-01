8f94624ee61e9bf6d2a0f9c1e677ed8a69b8f3d1
Richard <richard.ricciard@gmail.com>
2024-10-31 22:52:31 -0300

    finish

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const getStartedBtn = document.querySelector(".header__content .btn");
+getStartedBtn.addEventListener("click", () => {
+  alert("Let's Go!!!");
+});
+document.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll(".section__container");
+  const scrollTop = window.scrollY;
 
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (sectionTop + sectionHeight < scrollTop) {
+      section.style.opacity = "0.3";
+    } else {
+      section.style.opacity = "1";
+    }
+  });
+});
